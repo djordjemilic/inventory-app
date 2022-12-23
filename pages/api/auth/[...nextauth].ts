d0848639367dece8c124ee874c1e1a88e144a365
Djordje <djordjemilic33@gmail.com>
2022-12-23 19:33:39 +0100

    implemented authentication

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,44 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { prisma } from "../../../config/prisma";
+import { redirect } from "next/dist/server/api-utils";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "database",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    updateAge: 60 * 60 * 24, //24 hours
+  },
+
+  useSecureCookies: process.env.NODE_ENV === "production",
+
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+
+    async session({ session, token, user }) {
+      if (session?.user) session.user.id = user.id;
+      return session;
+    },
+  },
+
+  events: {},
+  debug: false,
+};
+
+export default NextAuth(authOptions);
---
+++ pages/auth/signin.tsx
@@ -0,0 +1,36 @@
+import { getProviders, signIn } from "next-auth/react";
+import { FaGoogle } from "react-icons/fa";
+import { Center, Button, Title, Stack, Text, Group } from "@mantine/core";
+
+export default function SignIn({ providers }: any) {
+  return (
+    <>
+      {Object.values(providers).map((provider: any) => (
+        <Center key={provider.name}>
+          <Stack spacing="xl">
+            <Title align="center">Inventory app</Title>
+            {provider.name === "Google" && (
+              <Button
+                onClick={() => signIn(provider.id)}
+                size="lg"
+                sx={{ alignSelf: "center" }}
+              >
+                <Group>
+                  <Text size="md">Sign in with {provider.name}</Text>
+                  <FaGoogle />
+                </Group>
+              </Button>
+            )}
+          </Stack>
+        </Center>
+      ))}
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
